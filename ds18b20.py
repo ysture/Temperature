9c7ec60d9f5dcc8dc75efdc2c6eeae97db3c95ee
Yngve <yngve.sture@gmail.com>
2020-03-02 17:47:53 +0100

    Fixed code

--- ds18b20.py
+++ ds18b20.py
@@ -4,10 +4,15 @@ from datetime import datetime
 import time
 
 def sensor():
+    ds18b20_list = []
     for i in os.listdir('/sys/bus/w1/devices'):
         if i != 'w1_bus_master1':
-            ds18b20 = i
-    return ds18b20
+            ds18b20_list.append(i)
+    return ds18b20_list
+
+sensors = {'28-01145ee16d0d':'1m',
+           '28-0300a279d522':'0m',
+           '28-01192836bd78':'3m'}
 
 def read(ds18b20):
     location = '/sys/bus/w1/devices/' + ds18b20 + '/w1_slave'
@@ -34,19 +39,21 @@ def user_input():
 response = user_input()
 '''
 
-def loop(ds18b20,frequency):
+def loop(ds18b20_list,frequency):
     while True:
-        if read(ds18b20) != None:
-            print("{time}: {celsius} C".format(time = read(ds18b20)[0],
-                                               celsius=read(ds18b20)[1]))
+        for ds18b20 in ds18b20_list:
+            if read(ds18b20) != None:
+                print("{time}: {celsius} C, {sensor}".format(time = read(ds18b20)[0],
+                                                             celsius=read(ds18b20)[1],
+                                                             sensor=sensors[ds18b20]))
 
-            '''
-            if response.lower() in {'c','celsius'}:
-                print("Current temperature : {celsius} C".format(celsius=read(ds18b20)[0]))
-            if response.lower() in {'f', 'fahrenheit'}:            
-                print("Current temperature : {fahr} F".format(fahr=read(ds18b20)[2]))
-            '''
-            time.sleep(frequency)
+                '''
+                if response.lower() in {'c','celsius'}:
+                    print("Current temperature : {celsius} C".format(celsius=read(ds18b20)[0]))
+                if response.lower() in {'f', 'fahrenheit'}:            
+                    print("Current temperature : {fahr} F".format(fahr=read(ds18b20)[2]))
+                '''
+        time.sleep(frequency)
             
 def kill():
     quit()
--- temp_db.py
+++ temp_db.py
@@ -1,7 +1,34 @@
+
 import firebase_admin
-'''
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
+from datetime import datetime
+
 
 cred = credentials.Certificate("./firebase_adminsdk.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+temp = 25.3
+sensor_id = '3m'
+time_stamp = datetime.now()
+
+doc_ref = db.collection(u'temperatures').document()
+doc_ref.set({
+    u'sensor-id':u'{}'.format(sensor_id),
+    u'temperature': u'{}'.format(temp),
+    u'timestamp': u'{}'.format(time_stamp.strftime("%Y-%m-%d %H:%M"))
+})
+
+'''
+import pyrebase
+
+config = {     
+  "apiKey": "AIzaSyDAc9vXb263-UxC8WKkiggpmfWB5k3qLHw",
+  "authDomain": "temperature-1d377.firebaseapp.com",
+  "databaseURL": "temperature-1d377.appspot.com",
+  "storageBucket": "https://temperature-1d377.firebaseio.com/temp%203m/8EIWbxJtYwm0srJzpewu"
+}
+
+firebase = pyrebase.initialize_app(config)
 '''
